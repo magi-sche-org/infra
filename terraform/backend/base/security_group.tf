780e097a4040d7dcd811d92f4f6bb7111cc15a02
Takuya Takahashi <pairs_char0j@icloud.com>
2024-02-24 19:26:45 +0900

    fix: add egress rule

--- terraform/backend/base/security_group.tf
+++ terraform/backend/base/security_group.tf
@@ -6,3 +6,21 @@ resource "aws_security_group" "server" {
     Name = "magische-${var.environment}-${var.service}-server"
   }
 }
+
+# allow egress
+resource "aws_security_group_rule" "server_egress_ipv4" {
+  security_group_id = aws_security_group.server.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+resource "aws_security_group_rule" "server_egress_ipv6" {
+  security_group_id = aws_security_group.server.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  ipv6_cidr_blocks  = ["::/0"]
+}

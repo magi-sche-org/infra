c5b9d7f7031e301d89ea0e36a1e73afa0ed20936
Takuya Takahashi <pairs_char0j@icloud.com>
2024-02-25 08:46:33 +0900

    chore: allow access cf->lb

--- terraform/base/security_group.tf
+++ terraform/base/security_group.tf
@@ -6,3 +6,17 @@ resource "aws_security_group" "lb" {
     Name = "magische-lb"
   }
 }
+
+# allow ingress from cloudfront
+data "aws_ec2_managed_prefix_list" "cloudfront" {
+  name = "com.amazonaws.global.cloudfront.origin-facing"
+}
+
+resource "aws_security_group_rule" "lb_ingress_cloudfront" {
+  security_group_id = aws_security_group.lb.id
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  prefix_list_ids   = [data.aws_ec2_managed_prefix_list.cloudfront.id]
+}

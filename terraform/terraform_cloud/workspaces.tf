8c48a76350d2c22ccc2d148d3b0dda794e9696b6
Takuya Takahashi <pairs_char0j@icloud.com>
2024-02-25 09:22:43 +0900

    fix: set module variable

--- terraform/terraform_cloud/workspaces.tf
+++ terraform/terraform_cloud/workspaces.tf
@@ -3,6 +3,7 @@ module "module_magische_base" {
   organization_name          = tfe_organization.magische_organization.name
   project_id                 = tfe_project.magische_project.id
   GITHUB_APP_INSTALLATION_ID = var.GITHUB_APP_INSTALLATION_ID
+  tfe_token_for_remote_state = var.TFE_TOKEN_FOR_REMOTE_STATE
   workspace_name             = "magische_infra_base"
   working_directory          = "terraform/base"
 }
@@ -12,6 +13,7 @@ module "module_magische_dev_frontend" {
   organization_name          = tfe_organization.magische_organization.name
   project_id                 = tfe_project.magische_project.id
   GITHUB_APP_INSTALLATION_ID = var.GITHUB_APP_INSTALLATION_ID
+  tfe_token_for_remote_state = var.TFE_TOKEN_FOR_REMOTE_STATE
   workspace_name             = "magische_infra_dev_frontend"
   working_directory          = "terraform/frontend/dev"
 }
@@ -21,6 +23,7 @@ module "module_magische_dev_backend" {
   organization_name          = tfe_organization.magische_organization.name
   project_id                 = tfe_project.magische_project.id
   GITHUB_APP_INSTALLATION_ID = var.GITHUB_APP_INSTALLATION_ID
+  tfe_token_for_remote_state = var.TFE_TOKEN_FOR_REMOTE_STATE
   workspace_name             = "magische_infra_dev_backend"
   working_directory          = "terraform/backend/dev"
 }
@@ -30,6 +33,7 @@ module "module_magische_prd_frontend" {
   organization_name          = tfe_organization.magische_organization.name
   project_id                 = tfe_project.magische_project.id
   GITHUB_APP_INSTALLATION_ID = var.GITHUB_APP_INSTALLATION_ID
+  tfe_token_for_remote_state = var.TFE_TOKEN_FOR_REMOTE_STATE
   workspace_name             = "magische_infra_prd_frontend"
   working_directory          = "terraform/frontend/prd"
 }
@@ -39,6 +43,7 @@ module "module_magische_prd_backend" {
   organization_name          = tfe_organization.magische_organization.name
   project_id                 = tfe_project.magische_project.id
   GITHUB_APP_INSTALLATION_ID = var.GITHUB_APP_INSTALLATION_ID
+  tfe_token_for_remote_state = var.TFE_TOKEN_FOR_REMOTE_STATE
   workspace_name             = "magische_infra_prd_backend"
   working_directory          = "terraform/backend/prd"
 }

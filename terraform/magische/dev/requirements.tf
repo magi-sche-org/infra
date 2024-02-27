5897236dec810a836a27ac08a39e92661493318a
Takuya Takahashi <pairs_char0j@icloud.com>
2024-02-27 12:29:19 +0900

    chore: acm

--- terraform/magische/dev/requirements.tf
+++ terraform/magische/dev/requirements.tf
@@ -18,6 +18,11 @@ terraform {
 provider "aws" {
   region = "ap-northeast-1"
 }
+provider "aws" {
+  alias  = "virginia"
+  region = "us-east-1"
+}
+
 # data.terraform_remote_state.base.outputs.で参照
 data "terraform_remote_state" "base" {
   backend = "remote"

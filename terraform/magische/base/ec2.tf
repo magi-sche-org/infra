cd29e6eb433c98de3eb981f24cabe543756482fb
Takuya Takahashi <pairs_char0j@icloud.com>
2024-03-01 22:49:59 +0900

    fix: subnet

--- terraform/magische/base/ec2.tf
+++ terraform/magische/base/ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "bastion" {
   ami                         = data.aws_ami.bastion.id
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.bastion.key_name
-  subnet_id                   = var.public_subnet_ids[0]
+  subnet_id                   = var.api_ecs_config.subnet_ids[0]
   associate_public_ip_address = true
   vpc_security_group_ids = [
     aws_security_group.bastion.id,

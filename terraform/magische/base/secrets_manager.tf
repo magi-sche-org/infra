26173a3cbe57f284225a402996ee03cac55dacee
Takuya Takahashi <pairs_char0j@icloud.com>
2024-03-01 13:42:17 +0900

    chore: expand ssm

--- terraform/magische/base/secrets_manager.tf
+++ terraform/magische/base/secrets_manager.tf
@@ -1,15 +1,15 @@
-resource "aws_secretsmanager_secret" "api_secret_key" {
-  name       = "magische-${var.env}-api-secret-key"
+resource "aws_secretsmanager_secret" "api_secret" {
+  name       = "magische-${var.env}-api-secret"
   kms_key_id = aws_kms_key.main.arn
   tags = {
-    Name = "magische-${var.env}-api-secret-key"
+    Name = "magische-${var.env}-api-secret"
   }
 }
 
 resource "aws_secretsmanager_secret_version" "api_secret_key" {
   secret_id = aws_secretsmanager_secret.api_secret_key.id
   secret_string = jsonencode({
-    api_secret_key = "please-change-me"
+    # api_secret_key = "please-change-me"
   })
 }
 

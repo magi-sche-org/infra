5387286b7015881f8f19db06fc419f17b546e698
Takuya Takahashi <pairs_char0j@icloud.com>
2024-03-02 13:32:35 +0900

    feat: vpc endpoint for ses

--- terraform/base/vpc.tf
+++ terraform/base/vpc.tf
@@ -307,3 +307,22 @@ resource "aws_vpc_endpoint" "ssm" {
     Name = "magische-ssm-endpoint"
   }
 }
+
+# ses(smtp)
+resource "aws_vpc_endpoint" "ses" {
+  vpc_id = aws_vpc.main.id
+
+  service_name      = "com.amazonaws.ap-northeast-1.email-smtp"
+  vpc_endpoint_type = "Interface"
+  subnet_ids = [
+    aws_subnet.private_1a.id,
+    aws_subnet.private_1c.id,
+    aws_subnet.private_1d.id
+  ]
+  private_dns_enabled = true
+  security_group_ids  = [aws_security_group.vpc_endpoint.id]
+
+  tags = {
+    Name = "magische-ses-endpoint"
+  }
+}

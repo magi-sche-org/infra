c2fe026bec5e31ec7b734b3d8b963276862cc50a
Takuya Takahashi <pairs_char0j@icloud.com>
2024-03-03 16:44:39 +0900

    chore: remove ses domain verification

--- terraform/magische/base/ses.tf
+++ terraform/magische/base/ses.tf
@@ -2,6 +2,14 @@ resource "aws_ses_domain_identity" "main" {
   domain = var.webfront_domain
 }
 
-resource "aws_ses_domain_identity_verification" "main" {
-  domain = aws_ses_domain_identity.main.domain
-}
+# resource "aws_ses_domain_identity_verification" "main" {
+#   domain = aws_ses_domain_identity.main.domain
+# }
+
+# resource "aws_route53_record" "ses_verification" {
+#   zone_id = var.route53_zone_id
+#   name    = aws_ses_domain_identity_verification.main.id
+#   type    = "TXT"
+#   ttl     = "60"
+#   records = [aws_ses_domain_identity_verification.main.verification_token]
+# }
